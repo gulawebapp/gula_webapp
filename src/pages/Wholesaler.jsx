f0dba8fe0a10cdafd66ef7cb919e14529b3de20f
isaacmatovu <matovuisaac646@gmail.com>
2025-03-28 16:09:23 +0300

    added user icon

--- src/pages/Wholesaler.jsx
+++ src/pages/Wholesaler.jsx
@@ -5,7 +5,6 @@ import SalesChart from "../components/Wholesaler/SalesChart/SalesChat";
 import TopProducts from "../components/Wholesaler/TopProducts/TopProducts";
 import RecentOrders from "../components/Wholesaler/RecentOrders/RecentOrders";
 
-
 export default function WholesalerDashboard() {
   const [showProductForm, setShowProductForm] = useState(false);
 
@@ -26,7 +25,7 @@ export default function WholesalerDashboard() {
         ) : (
           <>
             <MetricsGrid />
-            
+
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
               <div className="lg:col-span-2">
                 <SalesChart />
