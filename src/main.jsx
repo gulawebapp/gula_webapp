55a6220ad8f93399db85536db3dd7c6b80d32159
isaacmatovu <matovuisaac646@gmail.com>
2025-03-29 12:07:41 +0300

    added routes to common folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./components/routes";
+import routes from "./components/common/routes";
 
 const router = createBrowserRouter(routes);
 
