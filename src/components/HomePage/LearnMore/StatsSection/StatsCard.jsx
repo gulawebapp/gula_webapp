349ccb3254730f1d49b688e1f43011633d97dd84
isaacmatovu <matovuisaac646@gmail.com>
2025-04-07 17:45:20 +0300

    reduced card size

--- src/components/HomePage/LearnMore/StatsSection/StatsCard.jsx
+++ src/components/HomePage/LearnMore/StatsSection/StatsCard.jsx
@@ -1,22 +1,8 @@
-const StatsCard = ({ value, label, darkMode = false }) => {
+const StatsCard = ({ value, label }) => {
   return (
-    <div
-      className={`p-6 rounded-lg ${
-        darkMode ? "bg-purple-700" : "bg-white shadow-md"
-      }`}
-    >
-      <div
-        className={`text-4xl font-bold mb-2 ${
-          darkMode ? "text-white" : "text-purple-600"
-        }`}
-      >
-        {value}
-      </div>
-      <div
-        className={`text-lg ${darkMode ? "text-purple-100" : "text-gray-600"}`}
-      >
-        {label}
-      </div>
+    <div className="p-6 rounded-lg bg-purple-700">
+      <div className="text-4xl font-bold mb-2 text-white">{value}</div>
+      <div className="text-lg text-gray-600">{label}</div>
     </div>
   );
 };
