0db47cdf5d36c88df6972634d0b6974068905cf0
isaacmatovu <matovuisaac646@gmail.com>
2025-04-08 16:31:59 +0300

    changed background colour

--- src/components/HomePage/LearnMore/StatsSection/StatsCard.jsx
+++ src/components/HomePage/LearnMore/StatsSection/StatsCard.jsx
@@ -1,6 +1,6 @@
 const StatsCard = ({ value, label }) => {
   return (
-    <div className="p-6 rounded-lg bg-purple-700">
+    <div className="p-6 rounded-lg bg-gray-300">
       <div className="text-4xl font-bold mb-2 text-white">{value}</div>
       <div className="text-lg text-gray-600">{label}</div>
     </div>
