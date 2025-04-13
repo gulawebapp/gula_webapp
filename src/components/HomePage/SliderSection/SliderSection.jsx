b25675580ad05f8937bd0a8a4dd536dadcb628f9
isaacmatovu <matovuisaac646@gmail.com>
2025-04-13 19:19:47 +0300

    adjusted height and width of cards

--- src/components/HomePage/SliderSection/SliderSection.jsx
+++ src/components/HomePage/SliderSection/SliderSection.jsx
@@ -1,16 +1,39 @@
 import CategoryCard from "../CategoryCard/CategoryCard";
 import PropTypes from "prop-types";
 import styles from "./SliderSection.module.css";
-import electronics from "./images/electronics.webp";
-import fashion from "./images/fashion.webp";
-import goods from "./images/goods.webp";
-import cosmetics from "./images/cosmetics.webp";
+// Standard resolution imports
+import electronics1x from "./images/electronics-288.webp";
+import fashion1x from "./images/fashion-288.webp";
+import goods1x from "./images/goods-288.webp";
+import cosmetics1x from "./images/cosmetics-288.webp";
+
+// High resolution imports
+import electronics2x from "./images/electronics-576.webp";
+import fashion2x from "./images/fashion-576.webp";
+import goods2x from "./images/goods-576.webp";
+import cosmetics2x from "./images/cosmetics-576.webp";
 
 const categories = [
-  { name: "Electronics", image: electronics },
-  { name: "Fashion", image: fashion },
-  { name: "Home Goods", image: goods },
-  { name: "Beauty", image: cosmetics },
+  {
+    name: "Electronics",
+    image1x: electronics1x,
+    image2x: electronics2x,
+  },
+  {
+    name: "Fashion",
+    image1x: fashion1x,
+    image2x: fashion2x,
+  },
+  {
+    name: "Home Goods",
+    image1x: goods1x,
+    image2x: goods2x,
+  },
+  {
+    name: "Beauty",
+    image1x: cosmetics1x,
+    image2x: cosmetics2x,
+  },
 ];
 
 const SliderSection = () => {
