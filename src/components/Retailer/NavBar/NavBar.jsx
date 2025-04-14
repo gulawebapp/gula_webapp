c519a3661415695e32f5015f6a0f058b31f4b173
isaacmatovu <matovuisaac646@gmail.com>
2025-04-14 17:48:31 +0300

    resized logo for mobile

--- src/components/Retailer/NavBar/NavBar.jsx
+++ src/components/Retailer/NavBar/NavBar.jsx
@@ -41,10 +41,16 @@ export default function Navbar() {
 
           {/* Right-side Buttons */}
           <div className="flex flex-col md:flex-row md:items-center md:space-x-4 space-y-6 md:space-y-0">
-            <button className="p-2 text-gray-500 hover:text-gray-700">
+            <button
+              name="notification"
+              className="p-2 text-gray-500 hover:text-gray-700"
+            >
               <Bell className="w-5 h-5" />
             </button>
-            <button className="p-1 text-gray-500 hover:text-gray-700">
+            <button
+              name="user"
+              className="p-1 text-gray-500 hover:text-gray-700"
+            >
               <FaUser className="w-6 h-6 md:w-8 md:h-8 rounded-full" />
             </button>
             <button
