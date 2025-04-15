8e2e01891a3e8129d6e4006eeffe356a82ec0ee2
isaacmatovu <matovuisaac646@gmail.com>
2025-04-15 17:07:43 +0300

    removed loader from component

--- src/components/common/routes.jsx
+++ src/components/common/routes.jsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import Layout from "./Layout";
+import Loading from "./Loading";
 
 const HomePage = lazy(() => import("../../pages/HomePage"));
 const About = lazy(() => import("../../pages/About"));
@@ -12,11 +13,7 @@ const Account = lazy(() => import("../common/SubscriptionDetails"));
 const Learn = lazy(() => import("../../pages/LearnMore"));
 const LoginForm = lazy(() => import("../common/loginForm"));
 
-const Loading = () => (
-  <div className="flex justify-center items-center h-screen">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-  </div>
-);
+
 
 const routes = [
   {
