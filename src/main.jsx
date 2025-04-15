8db16b9ff52e39a0ed8ae4973af0405ab7db5034
isaacmatovu <matovuisaac646@gmail.com>
2025-04-15 17:07:10 +0300

    added tanstack query to routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SuspenseWrappedRoutes from "./components/common/routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter(SuspenseWrappedRoutes);
-
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
