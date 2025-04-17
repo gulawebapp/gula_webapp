4535ae1e48dce3d22a348aa2fce67fd7c05a370b
isaacmatovu <matovuisaac646@gmail.com>
2025-04-17 20:16:53 +0300

    edited routes file path

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SuspenseWrappedRoutes from "./components/common/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SuspenseWrappedRoutes from "./routes/routes";
 
 const router = createBrowserRouter(SuspenseWrappedRoutes);
 const queryClient = new QueryClient();
