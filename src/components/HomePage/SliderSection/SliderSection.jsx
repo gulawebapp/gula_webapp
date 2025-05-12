e43cf3ddadb8eac73613ff61dc4a4b4db71182bb
isaacmatovu <matovuisaac646@gmail.com>
2025-05-12 15:17:49 +0300

    increased slider width

--- src/components/HomePage/SliderSection/SliderSection.jsx
+++ src/components/HomePage/SliderSection/SliderSection.jsx
@@ -1,45 +1,36 @@
 import CategoryCard from "../CategoryCard/CategoryCard";
 import PropTypes from "prop-types";
 import styles from "./SliderSection.module.css";
-// Standard resolution imports
-import electronics1x from "./images/electronics-288.webp";
-import fashion1x from "./images/fashion-288.webp";
-import goods1x from "./images/goods-288.webp";
-import cosmetics1x from "./images/cosmetics-288.webp";
 
-// High resolution imports
-import electronics2x from "./images/electronics-576.webp";
-import fashion2x from "./images/fashion-576.webp";
-import goods2x from "./images/goods-576.webp";
-import cosmetics2x from "./images/cosmetics-576.webp";
+// Single image imports for each category
+import electronicsImg from "./images/electronics.webp";
+import fashionImg from "./images/fashion.webp";
+import goodsImg from "./images/goods.webp";
+import cosmeticsImg from "./images/cosmetics.webp";
 
 const categories = [
   {
     name: "Electronics",
-    image1x: electronics1x,
-    image2x: electronics2x,
+    image: electronicsImg,
   },
   {
     name: "Fashion",
-    image1x: fashion1x,
-    image2x: fashion2x,
+    image: fashionImg,
   },
   {
     name: "Home Goods",
-    image1x: goods1x,
-    image2x: goods2x,
+    image: goodsImg,
   },
   {
     name: "Beauty",
-    image1x: cosmetics1x,
-    image2x: cosmetics2x,
+    image: cosmeticsImg,
   },
 ];
 
 const SliderSection = () => {
   return (
     <section className="py-16 sm:py-20 bg-gray-50">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-2xl sm:text-3xl font-bold text-center mb-8 sm:mb-12">
           Featured Categories
         </h2>
@@ -56,6 +47,7 @@ const SliderSection = () => {
     </section>
   );
 };
+
 SliderSection.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
