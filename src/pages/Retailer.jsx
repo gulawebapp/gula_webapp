9f010aac83147af4b74f247f9332274c0f780a13
isaacmatovu <matovuisaac646@gmail.com>
2025-06-21 13:43:51 +0300

    made page responsive

--- src/pages/Retailer.jsx
+++ src/pages/Retailer.jsx
@@ -9,9 +9,15 @@ import CatalogUpdates from "../components/Retailer/CatalogUpdates/CatalogUpdates
 import ProductCarousel from "../components/Retailer/ProductCarousel/ProductCarousel";
 import Footer from "../components/Retailer/Footer/Footer";
 import ManageOrdersModal from "../components/Retailer/ManageOrders/ManageOrders";
+import Catalog from "../components/Retailer/Catalog";
 
 export default function RetailerDashboard() {
   const [isManageOrdersOpen, setIsManageOrdersOpen] = useState(false);
+  const [catalog, setCatalog] = useState(false);
+
+  const handleCatalog = () => {
+    setCatalog(!catalog);
+  };
 
   const openManageOrders = () => {
     setIsManageOrdersOpen(true);
@@ -24,19 +30,28 @@ export default function RetailerDashboard() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-
-      <main className="px-4 py-8 pl-32 sm:pl-6">
-        <DashboardHeader onManageOrdersClick={openManageOrders} />
-        <StatsGrid />
-
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-          <RecentOrders />
-          <CatalogUpdates />
+      <DashboardHeader
+        onManageOrdersClick={openManageOrders}
+        handleCatalog={handleCatalog}
+        catalog={catalog}
+      />
+      {catalog ? (
+        <div className="px-2 sm:px-6 pl-14">
+          <Catalog />
         </div>
+      ) : (
+        <main className="px-4 py-8 pl-32 sm:pl-20">
+          <StatsGrid />
 
-        <ProductCarousel />
-        <Footer />
-      </main>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <RecentOrders />
+            <CatalogUpdates />
+          </div>
+
+          <ProductCarousel />
+          <Footer />
+        </main>
+      )}
 
       <ManageOrdersModal
         isOpen={isManageOrdersOpen}
