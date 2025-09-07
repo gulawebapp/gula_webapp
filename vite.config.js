bc8b4c6bca3ac1a19fb13dd9bc809496179b3d16
isaacmatovu <matovuisaac646@gmail.com>
2025-09-07 14:20:32 +0300

    performed tree shaking

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          router: ["react-router-dom"],
+          firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
+          query: ["@tanstack/react-query"],
+          motion: ["framer-motion"],
+        },
+      },
+    },
+  },
 });
